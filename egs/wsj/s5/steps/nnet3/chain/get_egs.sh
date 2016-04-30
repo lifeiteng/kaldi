bfe743dafd2c67a6c319e1f7cc678b24adb2d230
Feiteng <feiteng@liulishuo.com>
2016-04-30 20:37:49 +0800

    decrease num_jobs, balance gpu useage

--- egs/wsj/s5/steps/nnet3/chain/get_egs.sh
+++ egs/wsj/s5/steps/nnet3/chain/get_egs.sh
@@ -58,10 +58,10 @@ left_tolerance=
 transform_dir=     # If supplied, overrides latdir as the place to find fMLLR transforms
 
 stage=0
-nj=15         # This should be set to the maximum number of jobs you are
+nj=6         # This should be set to the maximum number of jobs you are
               # comfortable to run in parallel; you can increase it if your disk
               # speed is greater and you have more machines.
-max_shuffle_jobs_run=50  # the shuffle jobs now include the nnet3-chain-normalize-egs command,
+max_shuffle_jobs_run=6  # the shuffle jobs now include the nnet3-chain-normalize-egs command,
                          # which is fairly CPU intensive, so we can run quite a few at once
                          # without overloading the disks.
 online_ivector_dir=  # can be used if we are including speaker information as iVectors.
@@ -131,7 +131,7 @@ num_lat_jobs=$(cat $latdir/num_jobs) || exit 1;
 # Get list of validation utterances.
 
 frame_shift=$(utils/data/get_frame_shift.sh $data)
-utils/data/get_utt2dur.sh $data
+[ ! -f $data/utt2dur ] && utils/data/get_utt2dur.sh $data
 
 cat $data/utt2dur | \
   awk -v min_len=$frames_per_eg -v fs=$frame_shift '{if ($2 * 1/fs >= min_len) print $1}' | \
