6ca487d5d0de9360ffc97d9a7cc42e3e6af4f73a
Feiteng <feiteng@liulishuo.com>
2016-05-04 14:38:09 +0800

    rm old version scripts

--- egs/openasr/path.sh
+++ egs/openasr/path.sh
@@ -1,5 +1,10 @@
-export KALDI_ROOT=/home/feiteng/kaldi-trunk #`pwd`/../../..
+if [ "`uname`" == "Linux" ]; then
+	export KALDI_ROOT=/home/feiteng/kaldi-trunk
+elif [ "`uname`" == "Darwin" ]; then
+	export KALDI_ROOT=/Users/feiteng/kaldi-trunk
+fi
+
 export PATH=$KALDI_ROOT/tools/sph2pipe_v2.5:$PWD/utils/:$KALDI_ROOT/src/bin:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/src/fstbin/:$KALDI_ROOT/src/gmmbin/:$KALDI_ROOT/src/featbin/:$KALDI_ROOT/src/lmbin/:$KALDI_ROOT/src/sgmmbin/:$KALDI_ROOT/src/sgmm2bin/:$KALDI_ROOT/src/fgmmbin/:$KALDI_ROOT/src/latbin/:$KALDI_ROOT/src/nnet2bin/:$KALDI_ROOT/src/nnetbin:$KALDI_ROOT/src/online2bin/:$KALDI_ROOT/src/ivectorbin/:$KALDI_ROOT/src/nnet3bin/:$PWD:$PATH
 export LC_ALL=C
 export IRSTLM=$KALDI_ROOT/tools/irstlm
-
+export PATH=$KALDI_ROOT/tools/srilm/bin:$KALDI_ROOT/tools/srilm/bin/i686-m64:$PATH
